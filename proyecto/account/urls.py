0e6873c92bc8bc3fb7aea03c96c1ded9da2fd46a
Gabriela Ovalle Motta <gaby26@umes.edu.gt>
2023-09-07 20:00:24 -0600

    Usuarios

--- proyecto/account/urls.py
+++ proyecto/account/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),
-    path('login/',views.login_view, name='login_view'),
-    path('registro',views.registro,name='registro'),
+   
 ]
--- proyecto/account/views.py
+++ proyecto/account/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
