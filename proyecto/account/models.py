c6984b23ecb049baa5f236d21bcdc06e7efc222c
Gabriela Ovalle Motta <gaby26@umes.edu.gt>
2023-09-07 23:06:59 -0600

    Vistas y token

--- proyecto/account/models.py
+++ proyecto/account/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from ..proyecto import settings
+import jwt
 # Create your models here.
 
 
@@ -7,3 +9,15 @@ class User(AbstractUser):
     is_admin=models.BooleanField('is_admin', default=False)
     is_customer=models.BooleanField('is_customer', default=False)
     is_employee=models.BooleanField('is_employee', default=False)
+
+def get_token(self):
+       
+
+        payload = {
+            'username': self.username,
+            'password': self.password,
+        }
+
+        token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+
+        return token
--- proyecto/account/views.py
+++ proyecto/account/views.py
@@ -1,6 +1,46 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate, login
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
-def index(request):
-    return render(request,'index.html')
+def registro(request):
+    model = User
+    template_name = 'registro.html'
+    fields = ['username', 'email', 'password1', 'password2']
+
+    def form_valid(self, form):
+        form.instance.is_customer = True
+        form.save()
+        return redirect('login')
+
+    form = UserCreationForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        return render(request, template_name, {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to different views based on the user's role
+            if user.is_admin:
+                return redirect('admin', {'token': user.get_token()})
+            elif user.is_customer:
+                return redirect('home', {'token': user.get_token()})
+            else:
+                return redirect('employee', {'token': user.get_token()})
+        else:
+            return render(request, 'login.html', {'error': 'nombre de usuario o correo invalidos'})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    logout(request)
+    return redirect('login')
